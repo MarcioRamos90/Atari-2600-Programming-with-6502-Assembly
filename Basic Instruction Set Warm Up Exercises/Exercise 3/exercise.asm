67a19236223c0052d23faf11245945087828487e
Márcio Ramos <marcioalvesramos90@hotmail.com>
2022-12-25 17:34:45 -0300

    My solution of exercise 4

--- Basic Instruction Set Warm Up Exercises/Exercise 3/exercise.asm
+++ Basic Instruction Set Warm Up Exercises/Exercise 3/exercise.asm
@@ -3,6 +3,7 @@
         org $F000         ; Define the origin of the ROM code at memory address $F000
 Start:
         lda #15           ; Load the A register with the literal decimal value 15
+
         tax               ; Transfer the value from A to X
         tay               ; Transfer the value from A to Y
         txa               ; Transfer the value from X to A
@@ -11,6 +12,8 @@ Start:
         ldx #6            ; Load X with the decimal value 6
         txa               ; Transfer the value from X to Y
         tay
+
+        jmp Start
 
         org $FFFC         ; End the ROM by adding required values to memory position $FFFC
         .word Start       ; Put 2 bytes with the reset address at memory position $FFFC
---
+++ Basic Instruction Set Warm Up Exercises/Exercise 4/exercise.md
@@ -0,0 +1,13 @@
+
+
+```
+Start:
+    ; TODO:
+    ... ....            ; Load the A register with the literal decimal value 100
+    ... ....            ; Add the decimal value 5 to the accumulator
+    ... ....            ; Subtract the decimal value 10 from the accumulator
+    ... ....            ; Register A should now contain the decimal 95 (or $5F in hexadecimal)
+
+End:
+    ...
+```
